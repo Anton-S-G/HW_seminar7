2818b68280f11acf5aeea98c24f0021db94b4e8c
Anton Sopov <recorrd@yandex.ru>
2024-02-03 01:04:28 +0300

    Change Task2

--- Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
+++ Task2/obj/Debug/net8.0/Task2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bc812c9ad62adbe0c3e05655869859615fa4ba0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d12b33c04e1d868f9af659f98ce013ad3509d20")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
