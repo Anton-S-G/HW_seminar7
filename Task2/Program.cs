2d12b33c04e1d868f9af659f98ce013ad3509d20
Anton Sopov <recorrd@yandex.ru>
2024-02-03 00:28:15 +0300

    Add decision to Task2

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Ack (int n, int m)
+{
+    while (n != 0) 
+    {
+        if (m == 0)
+        {
+            m = 1;
+        }
+        else
+        {
+            m = Ack (n, m - 1);
+        }
+        n--;
+    }
+    return m + 1;
+}
+Console.WriteLine(Ack(3,5));
