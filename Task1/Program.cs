a56350fe72e1e984bfc3b6fee069d709cbce692e
Anton Sopov <recorrd@yandex.ru>
2024-02-02 23:48:58 +0300

    Change Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,19 +2,7 @@
 int M = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int N = Convert.ToInt32(Console.ReadLine());
-// int start = 1;
-// start = 1, end = 5
-// void ShowNumbers(int start, int end)
-// {
-//     if (start == end)
-//     {
-//         Console.Write(start); 
-//         return; // Ломает всю программу, если попали в этот фрагмент 
-//     }
-//     Console.Write(start + " "); // 1 2 3 4 5
-//     ShowNumbers(start + 1, end);
-// }
-// ShowNumbers(M, N); // start = 1, end = N
+
 
 string ShowNumber (int start, int end)
 {
